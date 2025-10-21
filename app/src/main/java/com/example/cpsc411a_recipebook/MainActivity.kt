c6adda8b24059a9500a4f9cfa7667a73fa4b2fbb
Matthew Choi <matthewchoi774@gmail.com>
2025-10-20 18:15:28 -0700

    added neccesary files for repository: Base files and gradle files.

---
+++ app/src/main/java/com/example/cpsc411a_recipebook/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.cpsc411a_recipebook
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.cpsc411a_recipebook.ui.theme.CPSC411A_RecipeBookTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CPSC411A_RecipeBookTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CPSC411A_RecipeBookTheme {
+        Greeting("Android")
+    }
+}
